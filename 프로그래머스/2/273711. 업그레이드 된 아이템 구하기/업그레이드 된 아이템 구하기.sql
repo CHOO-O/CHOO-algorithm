8545e784363aeaab9a7b28c1fbcf88b93fac334c
CHOO-O <139360064+CHOO-O@users.noreply.github.com>
2024-10-25 20:40:35 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT I.ITEM_ID, I.ITEM_NAME, I.RARITY
+FROM ITEM_INFO I
+JOIN ITEM_TREE T
+ON I.ITEM_ID IN (SELECT ITEM_ID
+                 FROM ITEM_TREE
+                 WHERE PARENT_ITEM_ID IN (SELECT ITEM_ID
+                                          FROM ITEM_INFO
+                                          WHERE RARITY = 'RARE'))
+ORDER BY I.ITEM_ID DESC
+;
+
+# 어떤 아이템의 다음 업그레이드 아이템 -> PARENT_ITEM_ID로 이 아이템의 ID를 가지는
+#
