82cc3fccebdc9c0a18a198a5dc7b4dd43eb6f925
CHOO <139360064+CHOO-O@users.noreply.github.com>
2024-11-30 23:17:04 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 11.00 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,27 +1,15 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
-        char tempC = 't';
-        int tempI = 0;
+        char[] arr = my_string.toCharArray();
+        char temp = arr[num1];
         
-        if(num1 > num2){
-            tempI = num1;
-            num1 = num2;
-            num2 = tempI;
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        
+        for(char c : arr){
+            answer += c;
         }
-        
-        for(int i = 0; i < my_string.length(); i++){
-            if(i == num1){
-                tempC = my_string.charAt(i);
-                answer += "T";
-            } else if(i == num2){
-                answer += tempC;
-            } else {
-                answer += my_string.charAt(i);    
-            } 
-        }
-        
-        answer = answer.replace("T", String.valueOf(my_string.charAt(num2)));
         
         return answer;
     }
