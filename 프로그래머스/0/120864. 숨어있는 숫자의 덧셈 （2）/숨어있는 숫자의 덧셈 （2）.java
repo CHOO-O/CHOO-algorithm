53892a3f6b383db30e8d2bfa3c114d896824ba7b
CHOO <139360064+CHOO-O@users.noreply.github.com>
2024-12-18 16:58:50 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.18 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] num = my_string.split("[^0-9]+");
+        
+        for(String n : num){
+            if(!n.equals("")){
+                answer += Integer.valueOf(n);
+            }         
+        }
+
+        return answer;
+    }
+}
