02b37efc69f5ba1d07170c4cf95ff1e81cab3ced
CHOO <327cjm@naver.com>
2025-02-27 20:19:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.14 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,26 +4,24 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
+        // 더 나은 방법 : 
+        // 10진수 정수 값을 비트 연산하면 2진수 형태로 비트 연산을 수행한 뒤 10진수 형태로 저장하게 된다.
+        // 이를 다시 Integer.toBinaryString(str)을 사용해 2진수 변환한 string으로 변환해준다.
         for(int i = 0; i < n; i++){
-            String temp1 = Integer.toBinaryString(arr1[i]);
-            while(temp1.length() < n){
-                temp1 = "0" + temp1;
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+        }
+        
+        // replaceAll을 사용해 간단하게 치환해준다.
+        // 길이가 n이 되지 않을 경우를 반드시 고려한다.
+        for(int j = 0; j < n; j++){
+            String temp = answer[j];
+            temp = temp.replaceAll("1", "#");
+            temp = temp.replaceAll("0", " ");
+            while(temp.length() < n){
+                temp = " " + temp;
             }
             
-            String temp2 = Integer.toBinaryString(arr2[i]);
-            while(temp2.length() < n){
-                temp2 = "0" + temp2;
-            }
-                    
-            String temp3 = "";
-            for(int j = 0; j < n; j++){
-                if(temp1.charAt(j) == '0' && temp2.charAt(j) == '0'){
-                    temp3 += " ";
-                } else {
-                    temp3 += "#";
-                }
-            }
-            answer[i] = temp3;
+            answer[j] = temp;
         }
         
         return answer;
